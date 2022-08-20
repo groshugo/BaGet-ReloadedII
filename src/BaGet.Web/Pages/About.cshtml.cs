3d9108c43a4a985a297ea176b8a7a08abd6c9652
Sewer 56 <seweratwork@gmail.com>
2022-08-20 02:27:38 +0100

    Added: Website Content

---
+++ src/BaGet.Web/Pages/About.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BaGet.Web.Pages
+{
+    public class HomeModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
